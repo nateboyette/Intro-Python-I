afaee53e25662a17598c27aa2c88bb097e970ef2
Nate Boyette <nateboyette@icloud.com>
2019-04-22 15:06:13 -0700

    Completed printing.py

--- src/printing.py
+++ src/printing.py
@@ -12,6 +12,11 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+msg = "x is %s, y is %s, z is %s" % (x,y,z)
+
+print(msg)
 # Use the 'format' string method to print the same thing
+
+print(f"x is {x}, y is {y}, z is {z}")
 
 # Finally, print the same thing using an f-string
